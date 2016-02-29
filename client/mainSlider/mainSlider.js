34e08e36e29d934826f422b25b10d2c45558e84c
bazubrick <seregi123@mail.ru>
2016-03-01 00:41:39 +0200

    add arrow

--- client/mainSlider/mainSlider.js
+++ client/mainSlider/mainSlider.js
@@ -7,3 +7,12 @@ Template.mainSlider.events({
 
     }
 });
+Template.mainSlider.events({
+       'click .MainArrowBlock': function (e) {
+        e.preventDefault();
+        var destination = $('.tovar').offset().top;
+        var minus = destination - 60;
+        $("body,html").animate({scrollTop: minus }, 800);
+
+    }
+});
